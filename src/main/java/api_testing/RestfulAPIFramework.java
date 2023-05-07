6b5876bae3e14ccc8b93333b4e3e3946f75dd630
Ksenija Okuneva <ksokun@ttu.ee>
2023-05-07 20:08:33 +0300

    framework refactor

--- src/main/java/api_testing/RestfulAPIFramework.java
+++ src/main/java/api_testing/RestfulAPIFramework.java
@@ -12,15 +12,20 @@ public class RestfulAPIFramework {
     }
 
     public RequestSpecification createRequest(ContentType contentType) {
-        return RestAssured.given().contentType(contentType).header("Accept", "application/json");
+        return RestAssured.given()
+                .contentType(contentType)
+                .header("Accept", "application/json");
     }
 
     public Response sendRequest(RequestSpecification request, String httpMethod, String endpoint) {
-        return request.when().request(httpMethod, endpoint);
+        return request.when()
+                .request(httpMethod, endpoint);
     }
 
     public Response sendRequest(RequestSpecification request, String httpMethod, String endpoint, String acceptHeaderValue) {
-        return request.header("Accept", acceptHeaderValue).when().request(httpMethod, endpoint);
+        return request.header("Accept", acceptHeaderValue)
+                .when()
+                .request(httpMethod, endpoint);
     }
 
     public <T> void setPayload(RequestSpecification request, T payload) {
@@ -28,6 +33,7 @@ public class RestfulAPIFramework {
     }
 
     public void validateResponse(Response response, int expectedStatusCode) {
-        response.then().statusCode(expectedStatusCode);
+        response.then()
+                .statusCode(expectedStatusCode);
     }
 }
