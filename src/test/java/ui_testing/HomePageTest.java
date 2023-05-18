d9a7c5467808d18e371b5bb2a87a57a188a8d436
Ksenija Okuneva <ksokun@ttu.ee>
2023-05-18 23:56:12 +0300

    access for non-loggedin users is restricted

--- src/test/java/ui_testing/HomePageTest.java
+++ src/test/java/ui_testing/HomePageTest.java
@@ -2,9 +2,11 @@ package ui_testing;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.open;
 import static org.assertj.core.api.Assertions.assertThat;
 import static ui_testing.TestHelper.openHomePage;
 import static ui_testing.TestHelper.openInventoryPage;
@@ -37,10 +39,43 @@ public class HomePageTest {
         homePage.getLoginErrorMessage().should(Condition.visible);
     }
 
+    @Test
+    public void whenAccessedOtherPages_thenRedirectToLogin() {
+        String loginPageUrl = "https://www.saucedemo.com/";
+
+        open("https://www.saucedemo.com/inventory.html");
+        String currentUrl = getCurrentUrl();
+        assertThat(currentUrl).isEqualTo(loginPageUrl);
+
+        open("https://www.saucedemo.com/inventory-item.html?id=4");
+        currentUrl = getCurrentUrl();
+        assertThat(currentUrl).isEqualTo(loginPageUrl);
+
+        open("https://www.saucedemo.com/cart.html");
+        currentUrl = getCurrentUrl();
+        assertThat(currentUrl).isEqualTo(loginPageUrl);
+
+        open("https://www.saucedemo.com/checkout-step-one.html");
+        currentUrl = getCurrentUrl();
+        assertThat(currentUrl).isEqualTo(loginPageUrl);
+
+        open("https://www.saucedemo.com/checkout-step-two.html");
+        currentUrl = getCurrentUrl();
+        assertThat(currentUrl).isEqualTo(loginPageUrl);
+
+        open("https://www.saucedemo.com/checkout-complete.html");
+        currentUrl = getCurrentUrl();
+        assertThat(currentUrl).isEqualTo(loginPageUrl);
+    }
+
     @AfterEach
     public void clearAllAndCloseWindow() {
         Selenide.clearBrowserCookies();
         Selenide.clearBrowserLocalStorage();
         Selenide.closeWindow();
+    }
+
+    private String getCurrentUrl() {
+        return WebDriverRunner.url();
     }
 }
